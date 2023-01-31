e634ba5563406766464d9f18972f300f5b2a3710
Bing Xiang <bing.xiang@advancegroup.com>
2023-01-31 23:47:57 +0800

    support docker

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route("/ping")
+def ping():
+    return "pong"
+
 
 if __name__ == '__main__':
     app.run(None, 15000, True)
